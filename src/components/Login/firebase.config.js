827d3806c0aa4d5a29dd6bc7e0764746dd53ba14
mehedi345 <mehedihasan5101220@gmail.com>
2020-09-30 19:27:23 -0700

    final commit

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBHhOX8rtp2vahVcATkhTD_7CwARw6arrk",
-    authDomain: "fire-auth2.firebaseapp.com",
-    databaseURL: "https://fire-auth2.firebaseio.com",
-    projectId: "fire-auth2",
-    storageBucket: "fire-auth2.appspot.com",
-    messagingSenderId: "587131087182",
-    appId: "1:587131087182:web:f7e53e9007fe7760d7561f"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyCo9CroyMkJm5EXWal3TuycaIxT5w9dSL8",
+  authDomain: "ema-john-simple-92019.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-92019.firebaseio.com",
+  projectId: "ema-john-simple-92019",
+  storageBucket: "ema-john-simple-92019.appspot.com",
+  messagingSenderId: "580403099208",
+  appId: "1:580403099208:web:40f3a7a2ad54fe17a0ca7d"
+};
+
+export default firebaseConfig;
